7037cb3320394e51b626182a7a1acb8862b51b77
Anton Kurdin <anton.kurdin@gmail.com>
2017-10-11 01:45:45 +0300

    В файл quadratic_equation добавлены результаты функции get_roots при дискриминанте < 0 и a = 0. Дополнен файл README.

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,10 +2,19 @@ from math import sqrt
 
 
 def get_roots(a, b, c):
-    discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
-    if discriminant == 0:
-        return root1, None
-    else:
-        return root1, root2
+    if a == 0:
+        print ("There can't be 'a = 0' in quadratic equation")
+        return None 
+    else:   
+        discriminant = b ** 2 - 4 * a * c
+        if discriminant < 0:
+            root1 = None
+            root2 = None
+            return root1, root2
+        else:
+            root1 = (-b - sqrt(discriminant)) / (2 * a)
+            root2 = (-b + sqrt(discriminant)) / (2 * a)
+            if discriminant == 0:
+                return root1, None
+            else:
+                return root1, root2
